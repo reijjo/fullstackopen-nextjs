cb8d006cd5daf7e6d68f16b514553e1b5e879def
Reijjo <teemu.aitomeri@gmail.com>
2026-09-11 17:50:19 +0300

    c2_e5

--- blogs-app/src/app/blogs/page.tsx
+++ blogs-app/src/app/blogs/page.tsx
@@ -4,11 +4,13 @@ import { getBlogs } from "../services/blogs";
 export default function Blogs() {
   const blogs = getBlogs();
 
+  const mostLikesOnTop = blogs.sort((a, b) => b.likes - a.likes);
+
   return (
     <main>
       <h2>Blogs</h2>
       <ul>
-        {blogs.map((blog) => (
+        {mostLikesOnTop.map((blog) => (
           <li key={blog.id}>
             <Link href={`/blogs/${blog.id}`}>{blog.title}</Link>
             <p>
